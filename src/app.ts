f92decd94587803a9bdd64a1477e4c7938d62488
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2025-03-10 12:40:42 +0600

    initialize the project

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// application routes
+
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello, Pharma TypeScript (^_^)');
+};
+app.get('/', getAController);
+
+export default app;
